811b988259a4d30970c8bdaec69e99df0b105fd8
Saranganrajendran365 <saranganrajendran365@gmail.com>
2023-11-21 11:35:21 +0530

    added fiter based on city

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,25 +17,58 @@ import { HousingLocation } from '../housinglocation';
 
 export class HomeComponent {
   housingLocationList: HousingLocation[] = [];
-  // this  is to display the searched component
   filteredLocationList: HousingLocation[] = [];
+  filterCities: string[] = [];
   housingService: HousingService = inject(HousingService);
+  selectedCities: Set<string> = new Set();
+  private debounceTimer: any;
 
   constructor() {
+    // Fetch data and initialize filterCities
     this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
       this.housingLocationList = housingLocationList;
       this.filteredLocationList = housingLocationList;
+      this.updateFilterCities();
     });
   }
+
+  updateFilterCities() {
+    // Extract unique cities from the data
+    this.filterCities = Array.from(new Set(this.housingLocationList.map(location => location.city)));
+  }
+
+  onInputChange(text: string) {
+    clearTimeout(this.debounceTimer);
+
+    // Set a new timer for 300 milliseconds
+    this.debounceTimer = setTimeout(() => {
+      this.filterResults(text);
+    }, 1000);
+  }
+
   filterResults(text: string) {
-    console.log("text that we search is", text)
-    if (!text) {
+    if (!text && this.selectedCities.size === 0) {
       this.filteredLocationList = this.housingLocationList;
       return;
     }
 
-    this.filteredLocationList = this.housingLocationList.filter(
-      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase()) || housingLocation?.name.toLowerCase().includes(text.toLowerCase())
-    );
+    this.filteredLocationList = this.housingLocationList.filter((housingLocation) => {
+      const cityMatches = housingLocation?.city.toLowerCase().includes(text.toLowerCase());
+      const nameMatches = housingLocation?.name.toLowerCase().includes(text.toLowerCase());
+
+      return (text && (cityMatches || nameMatches)) || this.selectedCities.has(housingLocation.city);
+    });
   }
+
+  toggleCityFilter(city: string) {
+    if (this.selectedCities.has(city)) {
+      this.selectedCities.delete(city);
+    } else {
+      this.selectedCities.add(city);
+    }
+
+    this.filterResults('');
+  }
+
+
 }
